d5c7a0ff0309861ffbabb1cf2176a50575332ac2
Charles <c.friend85@yahoo.com>
2021-08-02 20:19:55 -0600

    Refactor intro stuff

--- Ruby_practice/Hashes_symbols/Night_at_the_Movies.rb
+++ Ruby_practice/Hashes_symbols/Night_at_the_Movies.rb
@@ -45,3 +45,42 @@ when "add"
     else
         puts "Error!"
     end
+
+
+
+    case choice
+    when "add"
+        puts "What movie would you like to add? "
+        title = gets.chomp
+        if movies[title.to_sym].nil? 
+        puts "What rating does the movie have? "
+            rating = gets.chomp
+            movies[title.to_sym] = rating.to_i
+        else
+            puts "That movie already exists! Its rating is #{movies[title.to_sym]}."
+        end
+    when "update"
+        puts "What movie would you like to update? "
+        title = gets.chomp
+        if movies[title.to_sym].nil? 
+            puts "That movie does not exist."
+        else
+            puts "What is the new rating? "
+            rating = gets.chomp
+            movies[title.to_sym] = rating.to_i
+        end
+    when "display"
+        movies.each do |movie, rating|
+            puts "#{movie}: #{rating}"
+        end
+    when "delete"
+        puts "What movie would you like to delete? "
+        title = gets.chomp
+        if movies[title.to_sym].nil? 
+            puts "That movie does not exist."
+        else
+            movies.delete(title.to_sym)
+        end
+    else
+        puts "Please enter a choice from the above methods"
+    end
---
+++ Ruby_practice/Refactoring/intro.rb
@@ -0,0 +1,7 @@
+# One line statements do not require an end
+
+puts "Yes!" if yes #one line if conditional
+
+puts "I'ts Ruby!" unless true #one line unless
+
+puts 100 < 101 ? "100 is less than 101" : "100 is greater than 101" #ternary operator
